9d5af009dbf67a273b2b644ccd724c3cce0f1f8e
almohammedrayyan <almohammedrayyan@gmail.com>
2023-02-13 19:04:43 +0530

    updated.

--- Frontend/my-web/src/component/Products/Products.jsx
+++ Frontend/my-web/src/component/Products/Products.jsx
@@ -1,44 +1,144 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import "./Products.css";
 import { useSelector, useDispatch } from "react-redux";
+import { clearError, getALLProduct } from "../../actions/productActions";
 import Loader from "../../layout/Loader";
-import { useAlert } from "react-alert";
-import {
-  getALLProduct,
-  getALLProductFilter,
-} from "../../actions/productActions";
 import ProductCard from "../Card/ProductCard";
-import Announcement from "../Announcemnet";
+import Pagination from "react-js-pagination";
+import Slider from "@material-ui/core/Slider";
+import { useAlert } from "react-alert";
+import Typography from "@material-ui/core/Typography";
+import MetaData from "../../layout/MetaData";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
-import "./Products.css";
+
+const categories = [
+  "Laptop",
+  "Footwear",
+  "Bottom",
+  "Tops",
+  "Attire",
+  "Camera",
+  "SmartPhones",
+];
 
 const ProductList = ({ match }) => {
   const dispatch = useDispatch();
 
-  const { products, loading } = useSelector((state) => state.products);
+  const alert = useAlert();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [price, setPrice] = useState([0, 25000]);
+  const [category, setCategory] = useState("");
+
+  const [ratings, setRatings] = useState(0);
+
+  const {
+    products,
+    loading,
+    error,
+    productCount,
+    resultPerPage,
+    filteredProductsCount,
+  } = useSelector((state) => state.products);
+
   const keyword = match.params.keyword;
 
+  const setCurrentPageNo = (e) => {
+    setCurrentPage(e);
+  };
+
+  const priceHandler = (event, newPrice) => {
+    setPrice(newPrice);
+  };
+  let count = filteredProductsCount;
+
   useEffect(() => {
-    dispatch(getALLProduct(keyword));
-  }, [dispatch, keyword]);
+    if (error) {
+      alert.error(error);
+      dispatch(clearError());
+    }
+
+    dispatch(getALLProduct(keyword, currentPage, price, category, ratings));
+  }, [dispatch, keyword, currentPage, price, category, ratings, alert, error]);
+
   return (
     <Fragment>
       {loading ? (
         <Loader />
       ) : (
         <Fragment>
-          <Announcement />
+          <MetaData title="PRODUCTS -- ECOMMERCE" />
           <Navbar />
+
           <h2 className="productsHeading">Products</h2>
+
           <div className="products">
             {products &&
               products.map((product) => (
-                <ProductCard key={product?._id} product={product} />
+                <ProductCard key={product._id} product={product} />
               ))}
           </div>
-          <Footer />
+
+          <div className="filterBox">
+            <Typography>Price</Typography>
+            <Slider
+              value={price}
+              onChange={priceHandler}
+              valueLabelDisplay="auto"
+              aria-labelledby="range-slider"
+              min={0}
+              max={25000}
+            />
+
+            <Typography>Categories</Typography>
+            <ul className="categoryBox">
+              {categories.map((category) => (
+                <li
+                  className="category-link"
+                  key={category}
+                  onClick={() => setCategory(category)}
+                >
+                  {category}
+                </li>
+              ))}
+            </ul>
+
+            <fieldset>
+              <Typography component="legend">Ratings Above</Typography>
+              <Slider
+                value={ratings}
+                onChange={(e, newRating) => {
+                  setRatings(newRating);
+                }}
+                aria-labelledby="continuous-slider"
+                valueLabelDisplay="auto"
+                min={0}
+                max={5}
+              />
+            </fieldset>
+          </div>
+          {resultPerPage < count && (
+            <div className="paginationBox">
+              <Pagination
+                activePage={currentPage}
+                itemsCountPerPage={resultPerPage}
+                totalItemsCount={productCount}
+                onChange={setCurrentPageNo}
+                nextPageText="Next"
+                prevPageText="Prev"
+                firstPageText="1st"
+                lastPageText="Last"
+                itemClass="page-item"
+                linkClass="page-link"
+                activeClass="pageItemActive"
+                activeLinkClass="pageLinkActive"
+              />
+            </div>
+          )}
         </Fragment>
       )}
+      <Footer />
     </Fragment>
   );
 };
